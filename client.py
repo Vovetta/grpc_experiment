2179878739f62aef47dc3bfd17ebf878c3a0b5ec
Vovetta <vkovyzin@yandex.ru>
2022-06-13 20:49:39 +0300

    All methods implemented

--- client.py
+++ client.py
@@ -8,25 +8,81 @@ response = services.Service.GreetingsUnaryUnary(
     'localhost:50051',
     insecure=True
 )
-print('Message: ' + response.message)
+print(f'Message: {response.message}')
 
 response = services.Service.GreetingsUnaryStream(
     protos.GreetingsRequest(name='you'),
     'localhost:50051',
     insecure=True
 )
-print('Message: ' + ', '.join(res.message for res in response))
+print(f'Message: {", ".join(res.message for res in response)}')
 
 response = services.Service.GreetingsStreamUnary(
     iter([protos.GreetingsRequest(name='Hi'), protos.GreetingsRequest(name='you')]),
     'localhost:50051',
     insecure=True
 )
-print('Message: ' + response.message)
+print(f'Message: {response.message}')
 
 response = services.Service.GreetingsStreamStream(
     iter([protos.GreetingsRequest(name='Hi'), protos.GreetingsRequest(name='you')]),
     'localhost:50051',
     insecure=True
 )
-print('Message: ' + ', '.join(res.message for res in response))
+print(f'Message: {", ".join(res.message for res in response)}')
+
+response = services.Service.MultiplyUnaryUnary(
+    protos.MultiplyRequest(number=5, multiplier=15),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {response.result}')
+
+response = services.Service.MultiplyUnaryStream(
+    protos.MultiplyRequest(number=5, multiplier=15),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {", ".join(str(res.result) for res in response)}')
+
+response = services.Service.MultiplyStreamUnary(
+    iter([protos.MultiplyRequest(number=5, multiplier=15), protos.MultiplyRequest(number=8, multiplier=13)]),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {response.result}')
+
+response = services.Service.MultiplyStreamStream(
+    iter([protos.MultiplyRequest(number=5, multiplier=15), protos.MultiplyRequest(number=8, multiplier=13)]),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {", ".join(str(res.result) for res in response)}')
+
+response = services.Service.FibonacciUnaryUnary(
+    protos.FibonacciRequest(number=5),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {response.result}')
+
+response = services.Service.FibonacciUnaryStream(
+    protos.FibonacciRequest(number=35),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {", ".join(str(res.result) for res in response)}')
+
+response = services.Service.FibonacciStreamUnary(
+    iter([protos.FibonacciRequest(number=5), protos.FibonacciRequest(number=7)]),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {response.result}')
+
+response = services.Service.FibonacciStreamStream(
+    iter([protos.FibonacciRequest(number=60), protos.FibonacciRequest(number=15)]),
+    'localhost:50051',
+    insecure=True
+)
+print(f'Message: {", ".join(str(res.result) for res in response)}')
--- python_server/server.py
+++ python_server/server.py
@@ -5,12 +5,30 @@ from concurrent import futures
 from py_grpc_prometheus.prometheus_server_interceptor import PromServerInterceptor
 from prometheus_client import start_http_server
 
-from service_pb2 import GreetingsResponse, GreetingsRequest
+from service_pb2 import (
+    GreetingsRequest, GreetingsResponse,
+    MultiplyRequest, MultiplyResponse,
+    FibonacciRequest, FibonacciResponse
+)
 from service_pb2_grpc import ServiceServicer, add_ServiceServicer_to_server
 
 
 SERVER_PORT = environ.get('PYTHON_SERVER_PORT', '50051')
 METRICS_PORT = int(environ.get('PYTHON_SERVER_METRICS_PORT', '1111'))
+
+
+def fibonacci(n):
+    if not (isinstance(n, int) and n >= 0):
+        raise ValueError(f'Positive integer number expected, got "{n}"')
+
+    if n in {0, 1}:
+        return n
+
+    previous, fib_number = 0, 1
+    for _ in range(2, n + 1):
+        previous, fib_number = fib_number, previous + fib_number
+
+    return fib_number
 
 
 class Service(ServiceServicer):
@@ -29,6 +47,38 @@ class Service(ServiceServicer):
     def GreetingsStreamStream(self, request_iterator: GreetingsRequest, context):
         for request in request_iterator:
             yield GreetingsResponse(message=request.name)
+
+    def MultiplyUnaryUnary(self, request: MultiplyRequest, context):
+        return MultiplyResponse(result=request.number * request.multiplier)
+
+    def MultiplyUnaryStream(self, request: MultiplyRequest, context):
+        yield MultiplyResponse(result=request.number * request.multiplier)
+
+    def MultiplyStreamUnary(self, request_iterator: MultiplyRequest, context):
+        message = []
+        for request in request_iterator:
+            message.append(request.number * request.multiplier)
+        return MultiplyResponse(result=message[-1])
+
+    def MultiplyStreamStream(self, request_iterator: MultiplyRequest, context):
+        for request in request_iterator:
+            yield MultiplyResponse(result=request.number * request.multiplier)
+
+    def FibonacciUnaryUnary(self, request: FibonacciRequest, context):
+        return FibonacciResponse(result=fibonacci(request.number))
+
+    def FibonacciUnaryStream(self, request: FibonacciRequest, context):
+        yield FibonacciResponse(result=fibonacci(request.number))
+
+    def FibonacciStreamUnary(self, request_iterator: FibonacciRequest, context):
+        message = []
+        for request in request_iterator:
+            message.append(fibonacci(request.number))
+        return FibonacciResponse(result=message[-1])
+
+    def FibonacciStreamStream(self, request_iterator: FibonacciRequest, context):
+        for request in request_iterator:
+            yield FibonacciResponse(result=fibonacci(request.number))
 
 
 def serve():
